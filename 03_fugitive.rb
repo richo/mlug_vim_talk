ca8e458100d345c2cfc7bfb1d2f0d6fab22370c9
richo <richo@psych0tik.net>
2011-11-17 22:30:32 +1100

    Added some docs for the actual talk

--- 03_fugitive.rb
+++ 03_fugitive.rb
@@ -1,5 +1,16 @@
 #!/usr/bin/env ruby
 #
+# Fugitive is a wrapper for git. I won't go too far into how it
+# works, purely because Tim Pipe writes /great/ documentation and
+# it's worth you checking out.
+#
+# But in brief, some of the cooler and less obvious features are
+#
+# Glog - Adds all revisions (or to a given rev) to the quickfix
+# list Ggrep - Adds all occurances of a string to the quickfix
+#
+# Worth noting that the unimpaired extension is VERY useful in this
+# context
 
 module Fugitive
   class FugitiveTest
