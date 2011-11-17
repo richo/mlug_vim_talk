3b653ef06bb010dc434dcb5ac93e7e9b4ffa8516
richo <richo@psych0tik.net>
2011-11-17 22:27:47 +1100

    Add some dummy methods so we'll have something to log

--- 03_fugitive.rb
+++ 03_fugitive.rb
@@ -4,5 +4,17 @@
 module Fugitive
   class FugitiveTest
 
+    def to_s
+      "#{super} as a string!"
+    end
+
+    def to_f
+      "#{super} as a float!"
+    end
+
+    def to_i
+      "#{super} as a Fixnum!"
+    end
+
   end
 end
