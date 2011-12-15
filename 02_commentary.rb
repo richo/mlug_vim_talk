654a759fb2c56bc3984af8bb67a5b48a86591d15
richo <richo@psych0tik.net>
2011-12-15 14:48:07 +1100

    Add some user content for commentary

--- 02_commentary.rb
+++ 02_commentary.rb
@@ -1,6 +1,18 @@
 #!/usr/bin/env ruby
 #
 # Test of commentary
+#
+# Commentary is an extraordinarily short, but exceedingly useful script by tpope
+# Essentially, it allows you to comment/uncomment large blocks easily. The most
+# useful feature is it's awareness of filetypes, for example wrapping large
+# chunks of haml in noop blocks.
+#
+# This is just a silly ruby script (which is fundamentally broken). Try
+# highlighting regions, and then pressing \\, or simply pressing \\\ to
+# comment/uncomment the current line.
+#
+# Commentary doesn't need any configuration out of the box, with it included
+# try some other filetypes!
 
 
 module Something
